d93fb3fd8e2e06d1867d62f4707d835b29583ef4
Amanda Matar <amandamatar@gmail.com>
2023-09-06 08:53:20 -0300

    primeiros códigos com typescript

---
+++ teste/helloworld.ts
@@ -0,0 +1,20 @@
+/*
+Java: String: nome
+TypeScript: nome: string
+*/
+
+function dizerOla(nome: string) {
+    console.log('Olá' + nome);
+    
+}
+
+dizerOla('Algaworks');
+
+/* novidade do ecs6
+
+function dizerOla(nome:string) {
+    console.log(`Olá ${nome}`);
+}
+dizerOla('Algaworks');
+
+*/
